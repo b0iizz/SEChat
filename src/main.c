67357ae5a83a995833c96c14bf91778b65f97233
Jonathan Grebe <jonathan@familiegrebe.de>
2024-12-18 21:24:47 +0100

    Start creating a simple test program to validate AL
    Just to make sure, that everything works as intended with our socketxp.h
    abstraction layer.

--- src/main.c
+++ src/main.c
@@ -3,10 +3,25 @@
 #include "socketxp.h"
 
 int main(void) {
-  if(sxp_init() != SXP_SUCCESS) {
-    printf("Error in initialization of socket abstraction layer!\n");
+  sxp_t socket;
+  sxpResult status;
+  if((status = sxp_init()) != SXP_SUCCESS) {
+    printf("Error in initialization of socket abstraction layer! %i\n", status);
+    return 1;
   }
   printf("sxp_init successful!\n");
+
+  if((status = sxp_create(&socket, AF_INET, SOCK_STREAM, 0, SXP_BLOCKING)) != SXP_SUCCESS) {
+    printf("Error while creating socket! %i\n", status);
+    return 1;
+  }
+
+  printf("Socket: %i\n", socket);
+
+  if((status = sxp_destroy(&socket)) != SXP_SUCCESS) {
+    printf("Error while destroying socket! %i\n", status);
+    return 1;
+  }
 
   sxp_cleanup();
   printf("sxp_cleanup\n");
