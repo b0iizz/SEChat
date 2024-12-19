b68d004d4e7be998e88ee7c1c3fbb6dc3033d0d2
Jonathan Grebe <jonathan@familiegrebe.de>
2024-12-19 09:37:55 +0100

    Add abstraction layer for terminal operations
    This is to create a cross platform terminal UI

---
+++ src/terminalxp.h
@@ -0,0 +1,37 @@
+#ifndef TERMINALXP_H_
+#define TERMINALXP_H_
+
+#if defined(WIN32) || defined(_WIN32) || defined(_WIN64)
+#else
+
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+typedef struct termios term_state_t;
+
+#endif /*WIN32*/
+
+typedef int txpResult;
+
+enum txpresults {
+  TXP_SUCCESS = 0,
+  TXP_TRY_AGAIN = 1,
+  TXP_ERROR = -1
+};
+
+txpResult txp_state_get(term_state_t *save);
+txpResult txp_state_set(term_state_t *save);
+
+txpResult txp_start();
+txpResult txp_stop();
+
+txpResult txp_send(const char *str);
+txpResult txp_recv(char *buffer, size_t *nread, size_t blen);
+txpResult txp_flush();
+
+#endif /*TERMINALXP_H_*/
