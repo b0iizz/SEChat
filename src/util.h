d5ad24095080a2e56f0b2810b176ff535e5759a4
Jonathan Grebe <jonathan@familiegrebe.de>
2025-01-23 19:38:32 +0100

    util: add util_split helper to split command arguments

--- src/util.h
+++ src/util.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int util_strcpy(char** dst, const char *src, int on_success, int on_failure);
+int util_strcpy(char **dst, const char *src, int on_success, int on_failure);
 int util_startswith(const char *str, const char *prefix);
+int util_split(char ***split, char *src);
 
 #endif /* UTIL_H_ */
